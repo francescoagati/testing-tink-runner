864f83bd49a8670165d4d825fda068db2ece4d20
francesco agati <francesco agati>
2019-04-21 21:04:32 +0200

    wip

---
+++ src/Main.hx
@@ -0,0 +1,74 @@
+import tink.testrunner.Runner;
+
+@:keep
+@:keepInit
+@:build(hx.doctest.DocTestGenerator.generateDocTests())
+class Main extends tink.testrunner.Suite.BasicSuite {
+
+	static function __init__() {
+		untyped __js__('global.window = {console:console}');
+	}
+
+    public static function main() {
+        Runner.run(new Main());
+    }
+
+    function new() {
+        super({name: Type.getClassName(Type.getClass(this))}, []);
+    }
+
+}
+
+
+
+class MyTools {
+
+    /**
+     * <pre><code>
+     * >>> MyTools.isValidName(null)   == false
+     * >>> MyTools.isValidName("")     == false
+     * >>> MyTools.isValidName("John") == true
+     * </code></pre>
+     */
+    public static function isValidName(str:String):Bool {
+        return str != null && str.length > 0;
+    }
+}
+
+
+class MyObject {
+
+    public var data:String;
+
+    /**
+     * <pre><code>
+     * >>> new MyObject(null) throws "[data] must not be null!"
+     * >>> new MyObject(null) throws ~/must not be null/
+     * >>> new MyObject("ab") throws nothing
+     * </code></pre>
+     */
+    public function new(data:String) {
+        if(data == null) throw "[data] must not be null!";
+        this.data = data;
+    }
+
+    /**
+     * <pre><code>
+     * >>> new MyObject("ab").length()  > 1
+     * >>> new MyObject("ab").length()  <= 2
+     * >>> new MyObject("abc").length() != 2
+     * </code></pre>
+     */
+    public function length():Int {
+        return data == null ? 0 : data.length;
+    }
+
+    /**
+     * <pre><code>
+     * >>> ({ var o=new MyObject("cat"); o.setData("dog"); o.data; }) == "dog"
+     * </code></pre>
+     */
+    public function setData(data:String):Void {
+        this.data = data;
+    }
+}
